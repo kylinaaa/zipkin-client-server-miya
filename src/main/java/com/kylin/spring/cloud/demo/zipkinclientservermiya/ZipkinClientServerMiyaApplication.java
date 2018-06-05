d2a1c9b46ba1d2bcce487c089d11e31c33edd186
kylinaaa <ys_1518802093@163.com>
2018-06-05 16:34:51 +0800

    message

---
+++ src/main/java/com/kylin/spring/cloud/demo/zipkinclientservermiya/ZipkinClientServerMiyaApplication.java
@@ -0,0 +1,49 @@
+package com.kylin.spring.cloud.demo.zipkinclientservermiya;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ZipkinClientServerMiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinClientServerMiyaApplication.class, args);
+	}
+
+	private static final Logger LOG = Logger.getLogger(ZipkinClientServerMiyaApplication.class.getName());
+
+	@RequestMapping("/hello")
+	public String home(){
+		LOG.log(Level.INFO, "hi is being called");
+		return "hi i'm miya!";
+	}
+
+	@RequestMapping("/miya")
+	public String info(){
+		LOG.log(Level.INFO, "info is being called");
+		return restTemplate.getForObject("http://localhost:8988/info2",String.class);
+	}
+	@RequestMapping("/mi")
+	public String in(){
+		LOG.log(Level.INFO, "info is being called");
+		return restTemplate.getForObject("http://localhost:8988/in2",String.class);
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+}
